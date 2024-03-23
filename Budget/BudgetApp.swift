6afb12e2141b7edb7ee6a6e8d23a277e5fe780bd
William Anderson <wanderson.wa252@gmail.com>
2024-03-23 14:17:12 -0400

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by Will Anderson on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
